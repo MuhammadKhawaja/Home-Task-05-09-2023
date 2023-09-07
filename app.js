6c38e9ac016a0bfa289f21cdd3bea57d4b0c1e07
Muhammad Khawaja <mdkhawaja2427@gmail.com>
2023-09-07 06:03:41 -0700

    two file added

---
+++ app.js
@@ -0,0 +1,191 @@
+// chapter 1 Home Task Alert
+
+
+// Q 1 a
+
+var firstName = "Muhammad";
+
+alert("First Name : " + firstName);
+
+// Q 1 b
+
+var lastName = "Khawaja";
+
+alert("Last Name : " + lastName);
+
+// Q 1 c
+
+var email = "muhammadkhawaja123@gmail.com";
+
+alert("Email : " + email);
+
+// Q 1 d
+
+var phoneNumber = "090078601";
+
+alert("Phone No : " + phoneNumber);
+
+// Q 1 e
+
+var password = "*********";
+
+alert("Password : " + password);
+
+// Q 2
+
+alert("You're learning JavaScript!");
+
+// Q 3
+
+alert("Hello there! Have a fantastic day!");
+
+
+// chapter 2 home task Variables For String 
+
+
+// Q 1
+
+var firstVariableName ="Muhammad Khawaja";
+
+// Q 2
+
+var web;
+
+web = "I am learning web development";
+
+// Q 3
+
+var teamName ="white House Boys";
+
+alert(teamName);
+
+// Q 4
+
+var bestMan = "Charlie";
+
+bestMan = "John";
+
+
+// chapter 3 Home Task Variables For Numbers
+
+
+// Q 1
+
+var caseQty;
+
+// Q 2
+
+var caseQty;
+
+caseQty = 144;
+
+// Q 3
+
+var num = 9;
+
+// Q 4
+
+var sum;
+
+sum = 4 + 4;
+
+console.log(sum);
+
+// Q 5
+
+var marchTotal = 100;
+
+var shippingCharge = 10;
+
+var orderTotal = marchTotal + shippingCharge;
+
+console.log(orderTotal);
+
+// Q 6
+
+var num = 20;
+
+num = num + 20;
+
+console.log(num);
+
+
+// chapter 4 Home Task Variable Names Legal And Illegal
+
+
+// Q 1
+
+var productCost = 3.45;
+
+// Q 2
+
+var nameOfBand;
+
+// Q 3
+
+var myMobileNumber = "03451444";
+
+// Q 4
+
+var myfullName = "Muhammad Khawaja";
+
+
+// Q 5 Legal Variables
+
+
+var mobileNumber;
+
+var _variableName;
+
+var myFirstPlace;
+
+
+// Q 5 Illegal variables
+
+
+console.log(" 1name , user name , user5name ");
+
+
+// chapter 5 Home Task Math Expression 
+
+
+// Q 1
+
+document.write(" <h1> Name - Modulus Operator , Symbol - % </h1> " )
+
+// Q 2
+
+var num9 = 20 % 6;
+
+console.log(num9);
+
+// Q 3
+
+var largeNum;
+
+var largeNum = 1000 * 2000;
+
+console.log(largeNum);
+
+// Q 4
+
+var number1 = 30;
+var number2 = 15;
+
+var result = number1 - number2;
+
+console.log (result);
+
+// Q 5
+
+var remainder1 = 15 % 10;
+
+console.log(remainder1)
+
+// Q 4
+
+var num5 = 4;
+var num6 = 8;
+var result = num5 * num6;
+
+alert("The result of multiplying " + num5 + " and "+ num6 + " is : " + result );
